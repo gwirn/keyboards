db4ba6b343b6a3f067513799f4c0de19cb01f855
gwirn <71886945+gwirn@users.noreply.github.com>
2025-03-18 08:30:23 +0100

    new keymap, rules, config, step instead of stl

--- handwired/iridiumfly/keymaps/gwirn/keymap.c
+++ handwired/iridiumfly/keymaps/gwirn/keymap.c
@@ -5,20 +5,32 @@
 
 
 const uint16_t PROGMEM combo1[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo2[] = {KC_S, KC_F, COMBO_END};
-const uint16_t PROGMEM combo3[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo2[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo3[] = {KC_K, KC_L, COMBO_END};
 
-combo_t key_combos[] = {
-    COMBO(combo1, KC_BSPC),
-    COMBO(combo2, KC_TAB),
-    COMBO(combo3, KC_ESC),
-};
+    combo_t key_combos[] = {
+        COMBO(combo1, KC_BSPC),
+        COMBO(combo2, KC_TAB),
+        COMBO(combo3, KC_ESC),
+    };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, HYPR(KC_SCLN), KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LALT(KC_SLSH), KC_LCTL, KC_LSFT, LT(3,KC_SPC), LT(2,KC_ENT), MO(1), KC_LGUI),
-    [1] = LAYOUT(KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TILD, KC_AT, KC_LPRN, KC_RPRN, KC_SLSH, KC_ASTR, KC_AMPR, KC_LT, KC_GT, KC_NO, KC_QUES, KC_EXLM, KC_LBRC, KC_RBRC, KC_NO, KC_COMM, KC_DOT, KC_EQL, KC_UNDS, KC_NO, KC_CIRC, KC_DLR, KC_PERC, KC_NO, KC_NO, KC_NO),
-    [2] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PIPE, KC_HASH, KC_MINS, KC_PLUS, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_GRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [3] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DQUO, KC_4, KC_5, KC_6, KC_QUOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_0, KC_NO, KC_NO)
+    const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+        [0] = LAYOUT(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P,
+                     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, MT(MOD_HYPR, KC_SCLN),
+                     KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_LALT, KC_SLSH),
+                     KC_LCTL, MO(3),MT(MOD_LSFT,KC_SPC), LT(2,KC_ENT), MO(1), KC_LGUI),
+        [1] = LAYOUT(KC_NO, KC_NO, KC_LCBR, KC_RCBR, LCTL(LALT(KC_ENT)), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                     KC_TILD, KC_AT, KC_LPRN, KC_RPRN, KC_EXLM, KC_ASTR, KC_AMPR, KC_EQL, KC_UNDS, KC_NO,
+                     KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                     KC_CIRC, KC_DLR, KC_PERC, KC_NO, KC_NO, KC_NO),
+        [2] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                     KC_PIPE, KC_HASH, KC_MINS, KC_PLUS, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_GRV,
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+        [3] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,LGUI(KC_TAB), KC_7, KC_8, KC_9, KC_NO,
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DQUO, KC_4, KC_5, KC_6, KC_QUOT,
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,KC_1, KC_2, KC_3, KC_NO,
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_0)
 };
 
 
